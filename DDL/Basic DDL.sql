a4600a5acb0507fef6004777df5f19b58aacfaac
Gur-E <gurmalhi@newmail.com>
2026-06-04 20:58:15 +0200

    Added create database statement

--- DDL/Basic DDL.sql
+++ DDL/Basic DDL.sql
@@ -1,2 +1,23 @@
 --Create
-USE MASTER GO;
+USE MASTER; 
+GO
+/*
+This is a SQL Server script that checks whether a database named SQLLEARNING exists, 
+forcibly disconnects all users from it, and then deletes the database.
+*/
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'SQLLEARNING')
+BEGIN
+--This changes the database access mode to SINGLE_USER.
+--Normally, SQL Server databases are in MULTI_USER mode, 
+--meaning many users and applications can connect at the same time.
+--cancels running queries, rolls back uncommitted transactions
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- Create <SQLLEARNING> database
+CREATE DATABASE DataWarehouse;
+GO
+
+
